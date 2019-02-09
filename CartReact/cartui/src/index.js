910f803a5608d58a25a652f87c80962e5981a37d
unknown <dhwanipandya1401@gmail.com>
2019-02-09 17:47:20 -0600

    Initial UI commit

---
+++ CartReact/cartui/src/index.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
